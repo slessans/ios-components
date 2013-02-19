9e59f94f33eef5a5418821723b5b386aef465637
Scott Lessans <scott@scottlessans.com>
2013-02-19 14:36:05 -0500

    Initial Commit

---
+++ Components/SCLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SCLAppDelegate.h
+//  Components
+//
+//  Created by Scott Lessans on 2/19/13.
+//  Copyright (c) 2013 Scott Lessans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Components/SCLViewController.h
@@ -0,0 +1,13 @@
+//
+//  SCLViewController.h
+//  Components
+//
+//  Created by Scott Lessans on 2/19/13.
+//  Copyright (c) 2013 Scott Lessans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCLViewController : UIViewController
+
+@end
