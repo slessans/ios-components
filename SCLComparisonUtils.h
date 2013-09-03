2d3beb138e0a5ab453f46cadf9d65adf89a1f429
Scott Lessans <scott@scottlessans.com>
2013-09-02 23:45:57 -0400

    added comparison utilities

---
+++ SCLComparisonUtils.h
@@ -0,0 +1,16 @@
+//
+//  SCLComparisonUtils.h
+//  Marquee
+//
+//  Created by Scott Lessans on 9/2/13.
+//  Copyright (c) 2013 Marquee Mobile. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+// compares two doubles
+extern const NSComparisonResult (^SCLDoubleCompare)(double, double);
+
+// compares two dates up to day-level granularity -- ie if two dates correspond
+// to the same day, they are equal.
+extern const NSComparisonResult (^SCLDayBasedDateCompare)(NSDate *, NSDate *);
---
+++ SCLComparisonUtils.m
@@ -0,0 +1,31 @@
+//
+//  SCLComparisonUtils.m
+//  Marquee
+//
+//  Created by Scott Lessans on 9/2/13.
+//  Copyright (c) 2013 Marquee Mobile. All rights reserved.
+//
+
+#import "SCLComparisonUtils.h"
+
+static const NSInteger SameDayDateComponents = NSYearCalendarUnit | NSMonthCalendarUnit |  NSDayCalendarUnit;
+
+const NSComparisonResult (^SCLDoubleCompare)(double, double) = ^(double int1, double int2){
+    if ( int1 < int2 ) return NSOrderedAscending;
+    if ( int1 > int2 ) return NSOrderedDescending;
+    return NSOrderedSame;
+};
+
+const NSComparisonResult (^SCLDayBasedDateCompare)(NSDate *, NSDate *) = ^(NSDate * date1, NSDate * date2){
+    
+    NSCalendar * currentCalendar = [NSCalendar currentCalendar];
+    
+    NSDateComponents * components1 = [currentCalendar components:SameDayDateComponents fromDate:date1];
+    NSDateComponents * components2 = [currentCalendar components:SameDayDateComponents fromDate:date2];
+    
+    NSComparisonResult result = SCLDoubleCompare([components1 year], [components2 year]);
+    if ( result == NSOrderedSame ) result = SCLDoubleCompare([components1 month], [components2 month]);
+    if ( result == NSOrderedSame ) result = SCLDoubleCompare([components1 day], [components2 day]);
+    
+    return result;
+};
