667cf92300283af836333604ff29b584d71b050b
Scott Lessans <scott@scottlessans.com>
2014-01-13 15:00:04 -0500

    added doc generation script for appledoc

---
+++ makeDocs.sh
@@ -0,0 +1,16 @@
+appledoc \
+--create-html \
+--no-create-docset \
+--project-name "SCL iOS Components" \
+--project-company "scottlessans.com" \
+--company-id "com.scottlessans" \
+--output "docs" \
+--logformat xcode \
+--keep-undocumented-objects \
+--keep-undocumented-members \
+--keep-intermediate-files \
+--no-repeat-first-par \
+--no-warn-invalid-crossref \
+--ignore "*.m" \
+--ignore "docs/*" \
+.
